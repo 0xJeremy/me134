783dc0b0679d0d396b3a6fc740e13456132928f2
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-09-25 19:25:18 -0400

    Added time converter and run loop

--- hw2/clock.py
+++ hw2/clock.py
@@ -1,78 +1,78 @@
 from adafruit_servokit import ServoKit
 
 # class ServoKit:
-# 	def __init__(self, channels=16, address=0x40):
-# 		self.servo = [i for i in range(14)]
+#   def __init__(self, channels=16, address=0x40):
+#       self.servo = [i for i in range(14)]
 
 class Segment:
-	def __init__(self, on, off, requirements=None):
-		self.on = on
-		self.off = off
-		self.requirements = requirements
+    def __init__(self, on, off, requirements=[]):
+        self.on = on
+        self.off = off
+        self.requirements = requirements
 
 # Channel -> (On location, off location)
 MAP = {
-	# Hours (tens)
-	0: Segment(0, 120),
-	1: Segment(100, 0),
-	2: Segment(75, 0),
-	3: Segment(100, 0),
-	4: Segment(115, 0),
-	5: Segment(10, 130),
-	6: Segment(140, 0),
-	# Hours (ones)
-	7: Segment(130, 0),
-	8: Segment(0, 120),
-	9: Segment(140, 0),
-	10: Segment(0, 140),
-	11: Segment(110, 0),
-	12: Segment(10, 100),
-	13: Segment(0, 120),
+    # Hours (tens)
+    0: Segment(0, 120),
+    1: Segment(100, 0),
+    2: Segment(75, 0),
+    3: Segment(100, 0),
+    4: Segment(115, 0),
+    5: Segment(10, 130),
+    6: Segment(140, 0),
+    # Hours (ones)
+    7: Segment(130, 0),
+    8: Segment(0, 120),
+    9: Segment(140, 0),
+    10: Segment(0, 140),
+    11: Segment(110, 0),
+    12: Segment(10, 100),
+    13: Segment(0, 120),
 
-	# Minutes (tens)
-	14: Segment(20, 120),
-	15: Segment(0, 130),
-	16: Segment(120, 0),
-	17: Segment(110, 0),
-	18: Segment(135, 5),
-	19: Segment(10, 120),
-	20: Segment(100, 0),
-	# Minutes (ones)
-	21: Segment(120, 0),
-	22: Segment(0, 120),
-	23: Segment(120, 0),
-	24: Segment(140, 10),
-	25: Segment(130, 0),
-	26: Segment(130, 10),
-	27: Segment(10, 130)
+    # Minutes (tens)
+    14: Segment(20, 120),
+    15: Segment(0, 130),
+    16: Segment(120, 0),
+    17: Segment(110, 0),
+    18: Segment(135, 5),
+    19: Segment(10, 120),
+    20: Segment(100, 0),
+    # Minutes (ones)
+    21: Segment(120, 0),
+    22: Segment(0, 120),
+    23: Segment(120, 0),
+    24: Segment(140, 10),
+    25: Segment(130, 0),
+    26: Segment(130, 10),
+    27: Segment(10, 130)
 }
 
 class Digit:
-	def __init__(self, driver, offset):
-		self.driver = driver
-		self.offset = 7 * offset
+    def __init__(self, driver, offset):
+        self.driver = driver
+        self.offset = 7 * offset
 
-	def set(self, binaryArray):
-		for i, segmentState in enumerate(binaryArray):
-			offset = self.offset + 1
-			segment = MAP[offset]
-			for requirement in segment.requirements:
-				# add logic to meet hardware requirements
-				pass
-			self.driver.servo[offset % 14].angle = segment.on if segmentState else segment.off
+    def set(self, binaryArray):
+        for i, segmentState in enumerate(binaryArray):
+            offset = self.offset + i
+            segment = MAP[offset]
+            for requirement in segment.requirements:
+                # add logic to meet hardware requirements
+                pass
+            self.driver.servo[offset % 14].angle = segment.on if segmentState else segment.off
 
 
 class Clock:
-	def __init__(self):
-		self.driverHours = ServoKit(channels=16, address=0x40)
-		self.driverMinutes = ServoKit(channels=16, address=0x40)
-		self.tensHour = Digit(self.driverHours, 0)
-		self.onesHour = Digit(self.driverHours, 1)
-		self.tensMinute = Digit(self.driverMinutes, 2)
-		self.onesMinute = Digit(self.driverMinutes, 3)
+    def __init__(self):
+        self.driverHours = ServoKit(channels=16, address=0x40)
+        self.driverMinutes = ServoKit(channels=16, address=0x41)
+        self.tensHour = Digit(self.driverHours, 0)
+        self.onesHour = Digit(self.driverHours, 1)
+        self.tensMinute = Digit(self.driverMinutes, 2)
+        self.onesMinute = Digit(self.driverMinutes, 3)
 
-	def set(self, binaryArray):
-		self.tensHour.set(binaryArray[0:7])
-		self.onesHour.set(binaryArray[7:14])
-		self.tensMinute.set(binaryArray[14:21])
-		self.onesMinute.set(binaryArray[21:28])
+    def set(self, binaryArray):
+        self.tensHour.set(binaryArray[0:7])
+        self.onesHour.set(binaryArray[7:14])
+        self.tensMinute.set(binaryArray[14:21])
+        self.onesMinute.set(binaryArray[21:28])
--- hw2/main.py
+++ hw2/main.py
@@ -1,2 +1,56 @@
 # 172.20.10.2 on iPhone
 
+from clock import Clock
+import time_conversion as tc
+import numpy as np
+import time
+
+def init():
+    clock = Clock()
+    time_binary_array = tc.get_binary_array()
+    clock.set(time_binary_array)
+    clock = 1
+
+    return clock, time_binary_array
+
+def main():
+    clock, previous_binary_array = init()
+
+    # Main loop
+    try:
+        while True:
+            time_binary_array = tc.get_binary_array()
+            if (np.array_equal(previous_binary_array, time_binary_array)):
+                pass
+            else:
+                clock.set(time_binary_array)
+                previous_binary_array = time_binary_array
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print("Closing program...")
+    finally:
+        print("Done.")
+
+# Loop through every value the clock can display
+def demonstrate(period=3):
+    clock, _ = init()
+
+    # Main loop
+    try:
+        # loop through all possible combinations with *period* pause between changes
+        for hour in range(24):
+            for minute in range(60):
+                binary_array = tc.time_to_binary_arrays(hour,minute)
+                print(tc.time_to_string(hour, minute))
+                print(binary_array)
+                clock.set(binary_array)
+                time.sleep(period)
+    except KeyboardInterrupt:
+        print("Closing program...")
+    finally:
+        print("Done.")
+
+
+if __name__ == "__main__":
+    # main()
+    demonstrate() 
