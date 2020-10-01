b06a96a444f0a65fc26308e824e965df110727c7
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-10-01 16:49:18 -0400

    Fixed abstraction barrier

--- hw3/main.py
+++ hw3/main.py
@@ -3,6 +3,8 @@ from pather import Pather
 from motion_planner import MotionPlanner
 
 RAISE = 'RAISE\n'
+HOP_HEIGHT = 0.5
+TRAVEL_TIME = 0.5
 
 def saveToFile(input, output):
     all_xs, all_ys = Pather(input).getPaths()
@@ -13,38 +15,37 @@ def saveToFile(input, output):
     for xs, ys in zip(all_xs, all_ys):
         path_x, path_y = planner.createMachinePath(xs, ys)
 
+        startingPosition = planner.getAngles(xs[0], ys[0], HOP_HEIGHT)
+        file.write('{} {} {}\n'.format(startingPosition[0], startingPosition[1], startingPosition[2]))
+
         for x, y in zip(path_x, path_y):
             angles = planner.getAngles(x, y)
             file.write('{} {} {}\n'.format(angles[0], angles[1], angles[2]))
-        file.write(RAISE)
+
+        endingPosition = planner.getAngles(xs[0], ys[0], HOP_HEIGHT)
+        file.write('{} {} {}\n'.format(endingPosition[0], endingPosition[1], endingPosition[2]))
 
     file.close()
 
 def parseFile(file):
     file = open(file, 'r')
-    tmp_angles = []
     all_angles = []
     for line in file:
-        if line == RAISE:
-            all_angles.append(tmp_angles)
-            tmp_angles = []
-            continue
         text = line.split(' ')
         angles = [float(angle) for angle in text]
-        tmp_angles.append(angles)
+        all_angles.append(angles)
 
-    # Returns a 3D Array. All Paths -> Single Path -> Angles at a Point
     return all_angles
 
 
 def drawFromFile(file):
 
-    file = open(file, 'r')
-    # robot = Robot()
-    for line in f:
-        text = line.split(' ')
-        angles = [float(angle) for angle in text]
-        print(angles)
+    robot = Robot()
+
+    all_angles = parseFile(file)
+
+    for angles in all_angles:
+        robot.goto(angles[0], angles[1], angles[2])
 
 if __name__ == '__main__':
     output = 'out.pos'
