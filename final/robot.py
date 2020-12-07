aa22dd6414a6b0f6c0927a6d23ac68072959f55a
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-12-07 10:24:39 -0500

    Added presentation, video, updated wall routine

--- final/robot.py
+++ final/robot.py
@@ -12,7 +12,6 @@ class Robot:
         self.legsEven = [self.legs[1], self.legs[3], self.legs[5]]
         setup(self.legs)
         self.forwardCount = 0
-        self.forwardTallCount = 0
         self.isTall = False
 
     def __walk(self, raiseAngle, turnAngle, direction, runtime):
@@ -46,6 +45,47 @@ class Robot:
         for leg in moveLegs:
             leg.knee.addAngle(stopOffset)
         actuate(moveLegs, runtime=runtime)
+
+    def raiseLegPair(self, pairNumber):
+        legPair = [pairNumber, pairNumber+3]
+
+        for leg in legPair:
+            self.leg(leg, knee=115, foot=20)
+            self.leg(leg, knee=135, foot=135)
+            # self.leg(leg, knee=135, foot=135)
+
+            if leg == 0 or leg == 5:
+                self.leg(leg, shoulder=30)
+
+            if leg == 2 or leg == 3:
+                self.leg(leg, shoulder=150)
+
+    def wallWalk(self, pairRaised, raiseAngle=34, turnAngle=14, runtime=0.07):
+        legSequence = {
+            0: [self.legs[1], self.legs[5], self.legs[2], self.legs[4]],
+            1: [self.legs[0], self.legs[5], self.legs[2], self.legs[3]],
+            2: [self.legs[0], self.legs[4], self.legs[1], self.legs[3]],
+        }[pairRaised]
+
+        for i in range(2):
+            for leg in legSequence:
+                # Raise leg
+                leg.knee.addAngle(-raiseAngle)
+                leg.foot.addAngle(-raiseAngle)
+                actuate([leg], runtime=runtime)
+
+                # Move forward
+                leg.shoulder.addAngle(turnAngle)
+                actuate([leg], runtime=runtime)
+
+                # Lower leg
+                leg.knee.addAngle(raiseAngle)
+                leg.foot.addAngle(raiseAngle)
+                actuate([leg], runtime=runtime)
+
+            for leg in legSequence:
+                leg.shoulder.addAngle(-turnAngle)
+                actuate([leg], runtime=runtime)
 
     def __turn(self, angle, direction, runtime):
         self.turnTick += 1
@@ -82,15 +122,9 @@ class Robot:
                 direction=-1,
                 runtime=runtime,
             )
-        if moveLegs is None and standLegs is None:
-            if not self.isTall:
-                self.forwardCount += 1
-                if self.forwardCount % 3:
-                    self.turnLeft()
-            else:
-                self.forwardTallCount += 1
-                if self.forwardTallCount % 3 == 1:
-                    self.turnLeft()
+        self.forwardCount += 1
+        if self.forwardCount % 3 == 2:
+            self.turnLeft()
 
     def backward(self, raiseAngle=34, turnAngle=14, runtime=0.03):
         for i in range(2):
@@ -100,14 +134,9 @@ class Robot:
                 direction=1,
                 runtime=runtime,
             )
-        if not self.isTall:
-            self.forwardCount += 1
-            if self.forwardCount % 3:
-                self.turnRight()
-        else:
-            self.forwardTallCount += 1
-            if self.forwardTallCount % 3 == 1:
-                self.turnRight()
+        self.forwardCount += 1
+        if self.forwardCount % 3:
+            self.turnRight()
 
     def turnRight(self, angle=20, runtime=0.05):
         for i in range(3):
@@ -136,7 +165,7 @@ class Robot:
             leg.foot.targetAngle = foot
         actuate(self.legs, runtime=runtime)
 
-    def leg(self, legNum, shoulder=None, knee=None, foot=None, runtime=0.05):
+    def leg(self, legNum, shoulder=None, knee=None, foot=None, runtime=0.05, actuation=True):
         leg = self.legs[legNum]
         if shoulder is not None:
             leg.shoulder.targetAngle = shoulder
@@ -144,7 +173,8 @@ class Robot:
             leg.knee.targetAngle = knee
         if foot is not None:
             leg.foot.targetAngle = foot
-        actuate(self.legs, runtime=runtime)
+        if actuation:
+            actuate(self.legs, runtime=runtime)
 
 
 if __name__ == "__main__":
--- final/routines.py
+++ final/routines.py
@@ -1,23 +1,116 @@
 import time
 
+def swimFrontLegs(robot):
+    robot.leg(0, knee=176, foot=101)
+    robot.leg(0, shoulder=30)
+    robot.leg(0, knee=210, foot=115)
 
-def __extend(robot, legNum):
-    robot.leg(legNum, knee=135, foot=135)
+    robot.leg(3, knee=176, foot=101)
+    robot.leg(3, shoulder=150)
+    robot.leg(3, knee=210, foot=115)
 
+    robot.leg(0, shoulder=90, actuation=False)
+    robot.leg(3, shoulder=90)
+
+def swimBackLegs(robot):
+    robot.leg(2, shoulder=150, actuation=False)
+    robot.leg(5, shoulder=30)
+
+    robot.leg(2, knee=176, foot=101)
+    robot.leg(2, shoulder=90)
+    robot.leg(2, knee=210, foot=115)
+
+    robot.leg(5, knee=176, foot=101)
+    robot.leg(5, shoulder=90)
+    robot.leg(5, knee=210, foot=115)
+
+    robot.leg(2, shoulder=150, actuation=False)
+    robot.leg(5, shoulder=30)
+
+def frontLegsForward(robot):
+    robot.leg(0, shoulder=30, actuation=False)
+    robot.leg(3, shoulder=150)
+
+def frontLegsBack(robot):
+    robot.leg(0, shoulder=90, actuation=False)
+    robot.leg(3, shoulder=90)
+
+def raiseFrontLegs(robot):
+    robot.leg(0, knee=176, foot=101, actuation=False)
+    robot.leg(3, knee=176, foot=101)
+
+def lowerFrontLegs(robot):
+    robot.leg(0, knee=210, foot=115, actuation=False)
+    robot.leg(3, knee=210, foot=115)
+
+def backLegsBack(robot):
+    robot.leg(2, shoulder=150, actuation=False)
+    robot.leg(5, shoulder=30)
+
+def backLegsForward(robot):
+    robot.leg(2, shoulder=90, actuation=False)
+    robot.leg(5, shoulder=90)
+
+def raiseBackLegs(robot):
+    robot.leg(2, knee=176, foot=101, actuation=False)
+    robot.leg(5, knee=176, foot=101)
+
+def lowerBackLegs(robot):
+    robot.leg(0, knee=210, foot=115, actuation=False)
+    robot.leg(3, knee=210, foot=115)
+
+
+def walkTall(robot):
+    pass
 
 def wall(robot):
-    robotLegs = robot.legs
-    __extend(robot, 0)  # raise front right
-    time.sleep(1)
-    robot.leg(0, shoulder=30)  # move front right forward
+    robot.goTall()
 
-    __extend(robot, 3)  # raise front left
+    # Raise front legs and brace on wall
+    robot.raiseLegPair(0)
     time.sleep(1)
-    robot.leg(3, shoulder=150)  # move front left forward
+    robot.raiseLegPair(1)
+    for leg in [0, 3]:
+        robot.leg(leg, knee=210, foot=115)
 
-    robot.forward(
-        moveLegs=[robotLegs[1], robotLegs[5]], standLegs=[robotLegs[2], robotLegs[4]]
-    )
+    time.sleep(0.1)
+
+    # Push forward from wall
+    robot.leg(0, shoulder=90, actuation=False)
+    robot.leg(3, shoulder=90)
+
+    robot.leg(2, shoulder=150, actuation=False)
+    robot.leg(5, shoulder=30)
+
+    swimFrontLegs(robot)
+    swimBackLegs(robot)
+
+    # robot.leg(0, shoulder=30, actuation=False)
+    # robot.leg(3, shoulder=150)
+
+
+    # Move middle legs forward
+    # robot.leg(1, shoulder=70, actuation=False)
+    # robot.leg(4, shoulder=110)
+
+    # robot.raiseLegPair(2)
+
+    # robot.leg(1, knee=210, foot=115, actuation=False)
+    # robot.leg(4, knee=210, foot=115)
+
+    # robot.leg(1, shoulder=90, actuation=False)
+    # robot.leg(4, shoulder=90)
+
+    # for i in range(3):
+    #     robot.raiseLegPair(0)
+    #     robot.wallWalk(0)
+
+    #     time.sleep(2)
+
+    # robot.wallWalk(2)
+    # robot.wallWalk(2)
+
+    # robot.goTall()
 
 
 if __name__ == "__main__":
